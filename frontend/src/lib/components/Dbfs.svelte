0215c6944af709d802c2d0442ff0f77c5b84db2d
Karsten Lehmann <mail@kalehmann.de>
2025-02-18 22:32:00 +0100

    feat: Show warning when server stops sending data

--- frontend/src/lib/components/Dbfs.svelte
+++ frontend/src/lib/components/Dbfs.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { dbfs2db } from "$lib/common.ts";
+    import Notification from "$lib/components/Notification.svelte";
 
     interface Props {
         reference?: number | null;
@@ -10,10 +11,18 @@
 
     const evtSource: EventSource = new EventSource("/sse");
     let dbfs: number = $state(-100.0);
+    let lastValueAt = $state(Date.now());
+    let now = $state(Date.now());
+
     evtSource.onmessage = (event) => {
         dbfs = parseFloat(event.data);
         value(dbfs);
+        lastValueAt = Date.now();
     };
+
+    setInterval(() => {
+        now = Date.now();
+    }, 100);
 </script>
 
 <span>
@@ -22,3 +31,11 @@
         ({dbfs2db(dbfs, reference).toFixed(2)} db<sub>Ref</sub>)
     {/if}
 </span>
+
+{#if now - lastValueAt > 2000}
+    <Notification isWarning>
+        Lost connection to server for
+        {((now - lastValueAt) / 1000).toFixed(1)}
+        seconds
+    </Notification>
+{/if}
---
+++ frontend/src/lib/components/Notification.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    interface Props {
+        children: Snippet;
+        isWarning?: boolean;
+    }
+
+    let { children, isWarning = false }: Props = $props();
+
+    let cssClasses = $derived.by(() => {
+        const classes = [
+            "m-5",
+            "p-5",
+            "rounded-lg",
+            "w-full",
+            "h-max",
+            "drop-shadow-lg",
+        ];
+
+        if (isWarning) {
+            classes.push("bg-pink-900");
+            classes.push("text-white");
+        } else {
+            classes.push("bg-white");
+        }
+
+        return classes.join(" ");
+    });
+</script>
+
+<div
+    class="w-screen
+           fixed
+           top-0
+           left-0
+           flex flex-row
+           justify-center"
+>
+    <div class={cssClasses}>
+        {@render children?.()}
+    </div>
+</div>
